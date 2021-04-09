6917f028ed28ca5895813a425e28a9bd2fb4543c
Kaushiki <kaushiki.agrawal01@gmail.com>
2021-04-09 20:28:40 +0530

    Commit 1

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project1.Models;
+namespace project1.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationUser _auc;
+        public UserController(ApplicationUser auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registration(User uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "The user " + uc.UserName + " is saved successfully";
+            return View();
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace project1.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Name..")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email..")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Please Enter Your Contact Number..")]
+        [Display(Name="Phone Number")]
+        [RegularExpression(@"^(\d{10})$", ErrorMessage = "Enter a valid phone number")]
+        public string PhoneNumber{  get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Password..")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Pwd { get; set; }
+
+        [Required(ErrorMessage = "Please Confirm Your Password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Pwd")]
+        public string Confirmpwd { get; set; }
+
+    
+    }
+}
